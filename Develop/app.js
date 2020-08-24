0bb3dd1c9af6e70395ccb171c46e0144cd09b4e6
George F <64555318+GAFelton@users.noreply.github.com>
2020-08-23 17:02:58 -0700

    Replaced comments with descriptive markup.

--- Develop/app.js
+++ Develop/app.js
@@ -1,3 +1,4 @@
+// IMPORT MODULES
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -5,16 +6,16 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+//Output path for the team.html final file.
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+//Empty repo to store generated employees before rendering the HTML.
 const employeeList = [];
 
+//Questions for user input.
 const questions = [{
     type: "input",
     name: "name",
@@ -83,6 +84,7 @@ const questions = [{
 }
 ];
 
+//Function for taking user input and using the Employee JS classes to convert it into the proper format.
 function convertToClass(data) {
 if (data.role === "Manager") {
     const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
@@ -98,6 +100,7 @@ if (data.role === "Intern") {
 }
 }
 
+//This function asks for user inputs, converts them to the correct Class object, then adds them to the employeeList array.
 async function enterEmployee() {
     const newEmployee = await inquirer
         .prompt(questions);
@@ -106,8 +109,8 @@ async function enterEmployee() {
     console.log("\n The above entry was added to the team list.");
     employeeList.push(employeeObj);
 }
-// enterEmployee();
 
+//This function handles question logic. It calls the enterEmployee function, then asks the user to either enter another Employee or to wrap up and render the final team.html document.
 async function readyToRender() {
     await enterEmployee();
     console.log(`Number of entries: ${employeeList.length}`)
@@ -136,31 +139,9 @@ async function readyToRender() {
     else {console.error(err);};
 }
 
+//The init function kicks off the application and calls the question logic function readyToRender().
 init = () => {
     console.log("Welcome to the Team page Generator! Enter employees in any order and this program will create a 'team.html' file in the output folder. Please enter your first employee.");
     readyToRender();
 }
 init();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
-// module.exports = App;
